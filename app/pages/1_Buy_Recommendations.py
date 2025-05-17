d82dd2ae86e1dd76e97984b9a14b65ea7656ebb6
Edson Nogueira <edson.nogueira@datavillas.com>
2025-05-17 02:06:58 -0300

    Improve margin of safety range.

--- app/pages/1_Buy_Recommendations.py
+++ app/pages/1_Buy_Recommendations.py
@@ -4,7 +4,7 @@ from helpers import recommends
 st.title("Buy Recommendations")
 
 margin_of_safety = st.slider(
-    "Enter you desired margin of safety: ", min_value=0.0, max_value=1.0, value=0.2
+    "Enter you desired margin of safety: ", min_value=0.1, max_value=0.5, value=0.2
 )
 
 st.dataframe(recommends(margin_of_safety, "buy"), hide_index=True)
