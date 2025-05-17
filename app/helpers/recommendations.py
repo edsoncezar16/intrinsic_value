f47de0367f59f5df398428464406e776568c5ea6
Edson Nogueira <edson.nogueira@datavillas.com>
2025-05-17 17:42:39 -0300

    Ensures streamlit tals ot motherduck.

--- app/helpers/recommendations.py
+++ app/helpers/recommendations.py
@@ -1,9 +1,17 @@
-import duckdb
 import streamlit as st
+from streamlit.connections import SQLConnection
+
+CACHE_TTL_SECONDS = 30.0
 
 
-@st.cache_data(ttl=30.0)
-def recommends(margin_of_safety, kind):
+@st.cache_data(ttl=CACHE_TTL_SECONDS)
+def recommends(
+    margin_of_safety: float,
+    kind: str,
+    conn: SQLConnection = st.connection(
+        name="intrinsic", type="sql", ttl=CACHE_TTL_SECONDS
+    ),
+):
     if kind == "buy":
         filter_condition = (
             f"WHERE market_price < intrinsic_value  * {1.0 - margin_of_safety}"
@@ -13,13 +21,12 @@ def recommends(margin_of_safety, kind):
     else:
         raise ValueError("Recommendation kind should be one of 'buy' or 'sell'.")
 
-    with duckdb.connect("../intrinsic.duckdb") as con:
-        data = con.execute(
-            f"""
+    return conn.query(
+        f"""
             SELECT * 
             FROM main_analytics.intrinsic_value
             {filter_condition}
             ORDER BY intrinsic_value {"DESC" if kind == "buy" else ""} 
-            """
-        ).fetch_df()
-    return data
+            """,
+        ttl=CACHE_TTL_SECONDS,
+    )
