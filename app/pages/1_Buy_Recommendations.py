b445338a4eac0a8b8c81ca04eb17981cfa831ee6
Edson Nogueira <edsoncezar16@gmail.com>
2025-05-17 21:06:50 -0300

    Add warnings to recommendations.

--- app/pages/1_Buy_Recommendations.py
+++ app/pages/1_Buy_Recommendations.py
@@ -3,6 +3,15 @@ from helpers import recommends
 
 st.title("Buy Recommendations")
 
+st.warning(
+    """
+    Take the following list at most as a starting point for you own analysis.
+    
+    Every investment decision should be thoughtful and you are the sole responsible for them.  
+    """,
+    icon="⚠️",
+)
+
 margin_of_safety = st.slider(
     "Enter you desired margin of safety: ", min_value=0.1, max_value=0.5, value=0.2
 )
