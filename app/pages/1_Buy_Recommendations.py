cc68561d6d0aaccd950ed8c76ac1ad3d09c85e54
Edson Nogueira <edsoncezar16@gmail.com>
2025-05-18 23:23:18 -0300

    Add total items info from recommendations lists.

--- app/pages/1_Buy_Recommendations.py
+++ app/pages/1_Buy_Recommendations.py
@@ -16,4 +16,7 @@ margin_of_safety = st.slider(
     "Enter you desired margin of safety: ", min_value=0.1, max_value=0.5, value=0.2
 )
 
-st.dataframe(recommends(margin_of_safety, "buy"), hide_index=True)
+buy_recommends = recommends(margin_of_safety, "buy")
+
+st.write(f"Found {buy_recommends.shape[0]} buy recommendations:")
+st.dataframe(buy_recommends, hide_index=True)
--- app/pages/2_Sell_Recommendations.py
+++ app/pages/2_Sell_Recommendations.py
@@ -16,4 +16,7 @@ margin_of_safety = st.slider(
     "Enter you desired margin of safety: ", min_value=0.1, max_value=0.5, value=0.2
 )
 
-st.dataframe(recommends(margin_of_safety, "sell"), hide_index=True)
+sell_recommends = recommends(margin_of_safety, "sell")
+
+st.write(f"Found {sell_recommends.shape[0]} sell recommendations:")
+st.dataframe(sell_recommends, hide_index=True)
