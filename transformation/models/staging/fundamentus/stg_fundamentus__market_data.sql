6a76fb41bdb55ceea25bde52b039338418d4a3d2
Edson Nogueira <edsoncezar16@gmail.com>
2025-05-19 11:30:08 -0300

    Fix type 3 name issue for Monark.

--- transformation/models/staging/fundamentus/stg_fundamentus__market_data.sql
+++ transformation/models/staging/fundamentus/stg_fundamentus__market_data.sql
@@ -13,6 +13,7 @@ SELECT
         WHEN company_name = 'OD' THEN 'ODONTOPREV' -- type 3
         WHEN company_name = 'PETROREC' THEN 'PETRORECONCA' -- type 3
         WHEN company_name = 'WILS' THEN 'WILSON SONS' -- type 3
+        WHEN company_name = 'M' THEN 'MONARK' -- type 3
         ELSE company_name
     END AS company_name,
     industry,
