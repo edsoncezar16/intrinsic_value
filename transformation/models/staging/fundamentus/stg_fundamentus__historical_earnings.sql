04d78c0fbe0b439e021ec41e4e9ae8fa9d563644
Edson Nogueira <edsoncezar16@gmail.com>
2025-05-20 00:31:44 -0300

    Fix naming issue for ceee-d.

--- transformation/models/staging/fundamentus/stg_fundamentus__historical_earnings.sql
+++ transformation/models/staging/fundamentus/stg_fundamentus__historical_earnings.sql
@@ -1,14 +1,13 @@
 SELECT
-    -- Fixes inconsistencies at the source:
-    ---- type 1: mismatch between company names scraped from the website and from the balance sheets
-    ---- type 2: wrong stock type on website
-    ---- type 3: company name contains "ON", which will conflict with scraping rules
+    -- Fixes inconsistencies at the source: company name contains "-",
+    -- which will conflict with balance sheet parsing rules
     CASE
-        WHEN company_name = 'IOCHPE' THEN 'IOCHPE-MAXION' -- type 1
-        WHEN company_name = 'FRAS' THEN 'FRAS-LE S.A.' -- type 1
-        WHEN company_name = 'CEB' THEN 'CEB - COMPANHIA ENERGÉTICA DE BRASÍLIA' -- type 1
-        WHEN company_name = 'Log' THEN 'Log-In' -- type 1
+        WHEN company_name = 'IOCHPE' THEN 'IOCHPE-MAXION'
+        WHEN company_name = 'FRAS' THEN 'FRAS-LE S.A.'
+        WHEN company_name = 'CEB' THEN 'CEB - COMPANHIA ENERGÉTICA DE BRASÍLIA'
+        WHEN company_name = 'Log' THEN 'Log-In'
         WHEN company_name = 'QUERO' THEN 'QUERO-QUERO'
+        WHEN company_name = 'CEEE' THEN 'CEEE-D'
         ELSE company_name
     END AS company_name,
     CAST(
