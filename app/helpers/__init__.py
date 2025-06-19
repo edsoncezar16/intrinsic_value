bae7c287bb9a03966a6b62f51a40661ebc5a25f4
Edson <edsoncezar16@gmail.com>
2025-06-19 11:14:45 -0300

    Fix helpers module.

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,15 +1,6 @@
 from .recommendations import recommends
-from .analysis import (
-    get_total_companies,
-    get_average_price,
-    get_average_value,
-    get_analysis_data,
-)
+
 
 __all__ = [
     "recommends",
-    "get_total_companies",
-    "get_average_price",
-    "get_average_value",
-    "get_analysis_data",
 ]
