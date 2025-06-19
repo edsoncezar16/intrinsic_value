0696ed8d995416999ca3c7b0a0b3a59d541f2207
Edson <edsoncezar16@gmail.com>
2025-06-19 15:45:13 -0300

    Improve debugging logs in extraction pipeline.

--- ingestion/scraping_pipeline.py
+++ ingestion/scraping_pipeline.py
@@ -1,6 +1,8 @@
 import dlt
 from bs4 import BeautifulSoup
 from typing import Iterator, Any
+import dlt.extract
+import dlt.extract.exceptions
 from dlt.sources.helpers import requests
 
 FUNDAMENTUS_BASE_URL: str = "https://www.fundamentus.com.br/detalhes.php?papel="
@@ -15,22 +17,30 @@ def get_relevant_info(all_info: list[str], relevant_info: str) -> str:
 @dlt.resource(standalone=True, max_table_nesting=0, write_disposition="replace")
 def market_data(tickers: list[str] = dlt.config.value) -> Iterator[dict[str, Any]]:
     for ticker in tickers:
-        response = requests.get(FUNDAMENTUS_BASE_URL + ticker)
-        soup = BeautifulSoup(response.text, "html.parser")
-        all_info = list(map(lambda x: x.text, soup.find_all("span", "txt")))
-        company_name = get_relevant_info(all_info, "Empresa").split("ON")[0].strip()
-        n_stocks = int(get_relevant_info(all_info, "Nro. Ações").replace(".", ""))
-        market_price = float(get_relevant_info(all_info, "Cotação").replace(",", "."))
-        market_price_date = get_relevant_info(all_info, "Data últ cot")
-        industry = get_relevant_info(all_info, "Subsetor")
-        yield {
-            "ticker": ticker,
-            "company_name": company_name,
-            "industry": industry,
-            "n_stocks": n_stocks,
-            "market_price": market_price,
-            "market_price_date": market_price_date,
-        }
+        try:
+            response = requests.get(FUNDAMENTUS_BASE_URL + ticker)
+            soup = BeautifulSoup(response.text, "html.parser")
+            all_info = list(
+                map(lambda x: x.text, soup.find_all("span", {"txt": "txt"}))
+            )
+            company_name = get_relevant_info(all_info, "Empresa").split("ON")[0].strip()
+            n_stocks = int(get_relevant_info(all_info, "Nro. Ações").replace(".", ""))
+            market_price = float(
+                get_relevant_info(all_info, "Cotação").replace(",", ".")
+            )
+            market_price_date = get_relevant_info(all_info, "Data últ cot")
+            industry = get_relevant_info(all_info, "Subsetor")
+            yield {
+                "ticker": ticker,
+                "company_name": company_name,
+                "industry": industry,
+                "n_stocks": n_stocks,
+                "market_price": market_price,
+                "market_price_date": market_price_date,
+            }
+        except dlt.extract.exceptions.ResourceExtractionError as e:
+            print(f"Failed to extract data for ticker {ticker}: {e}")
+            raise e
 
 
 def scrape_fundamentus() -> None:
