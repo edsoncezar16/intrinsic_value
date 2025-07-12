8268caebc2ffcf6a8abdb4f5faa393408c8cc36a
Edson <edsoncezar16@gmail.com>
2025-07-12 08:45:27 -0300

    chore: ensure tests

--- transformation/models/staging/google_sheets/stg_google_sheets__financial_data.sql
+++ transformation/models/staging/google_sheets/stg_google_sheets__financial_data.sql
@@ -1,16 +1,12 @@
 SELECT
     stock,
-    CAST(
-        dividends AS FLOAT
-    ),
-    CAST(
-        earnings AS FLOAT
-    ),
-    CAST(
-        roe AS FLOAT
-    )
+    dividends,
+    earnings,
+    roe
 FROM
     {{ source(
         'trading_view',
         'financial_info'
     ) }}
+WHERE
+    skipped IS NULL -- this filters out the metadata row added by dlt
