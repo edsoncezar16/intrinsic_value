a6870bfe6443fb0daabbde19e9f9127e6bd6e0ae
Edson <edsoncezar16@gmail.com>
2025-07-13 19:45:11 -0300

    fix: resources setup for branch deployments

--- orchestration/definitions.py
+++ orchestration/definitions.py
@@ -15,13 +15,9 @@ from dagster_dbt import DbtCliResource
 from dagster_duckdb import DuckDBResource
 import os
 
-is_branch_deployment: bool = (
-    os.environ.get("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT", "") == "1"
-)
+jobs = [data_ingestion_job]
 
-base_jobs = [data_ingestion_job]
-
-base_resources = {
+resources = {
     "dlt": dlt_resource,
     "dbt": DbtCliResource(project_dir=intrinsic_project),
 }
@@ -29,17 +25,12 @@ base_resources = {
 branch_deployment_jobs = [clone_prod.to_job(), drop_prod_clone.to_job()]
 
 motherduck_resource = DuckDBResource(
-    database=f"md:{os.environ.get('DAGSTER_CLOUD_DEPLOYMENT_NAME', '')}_{os.environ.get('BASE_DB_NAME', '')}?motherduck_token={os.environ.get('MOTHERDUCK_TOKEN')}"
+    database=f"md:{os.environ.get('MOTHERDUCK_DATABASE', '')}_clone_{os.environ.get('DAGSTER_CLOUD_PULL_REQUEST_ID', '')}?motherduck_token={os.environ.get('MOTHERDUCK_TOKEN')}"
 )
 
-
-jobs = base_jobs + branch_deployment_jobs if is_branch_deployment else base_jobs
-
-resources = (
-    base_resources.update({"motherduck": motherduck_resource})
-    if is_branch_deployment
-    else base_resources
-)
+if os.environ.get("DAGSTER_CLOUD_IS_BRANCH_DEPLOYMENT", "") == "1":
+    jobs = jobs + branch_deployment_jobs
+    resources.update({"motherduck": motherduck_resource})
 
 defs = Definitions(
     assets=[dagster_financial_assets, dagster_market_assets, intrinsic_dbt_assets],
