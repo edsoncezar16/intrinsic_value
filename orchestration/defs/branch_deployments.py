aea4667a873fa7048f79368dddd516d10ab9c8b9
Edson <edsoncezar16@gmail.com>
2025-07-13 22:40:37 -0300

    chore: clone database name improvement

--- orchestration/defs/branch_deployments.py
+++ orchestration/defs/branch_deployments.py
@@ -3,14 +3,14 @@ from dagster import In, Nothing, graph, op, OpExecutionContext
 import os
 
 base_dbname: str = os.environ.get("MOTHERDUCK_DATABASE", "")
-pr_id: str = os.environ.get("DAGSTER_CLOUD_PR_ID", "")
+pr_id: str = os.environ.get("DAGSTER_CLOUD_PULL_REQUEST_ID", "")
 
 
 @op
 def drop_database_clone(motherduck: DuckDBResource):
     with motherduck.get_connection() as conn:
         cur = conn.cursor()
-        cur.execute(f"DROP DATABASE IF EXISTS {base_dbname}_clone{pr_id}")
+        cur.execute(f"DROP DATABASE IF EXISTS {base_dbname}_clone_{pr_id}")
 
 
 @op(ins={"start": In(Nothing)})
@@ -18,7 +18,7 @@ def clone_production_database(context: OpExecutionContext, motherduck: DuckDBRes
     with motherduck.get_connection() as conn:
         cur = conn.cursor()
         cur.execute(
-            f"CREATE DATABASE {base_dbname}_clone{pr_id} FROM CURRENT_DATABASE()"
+            f"CREATE DATABASE {base_dbname}_clone_{pr_id} FROM CURRENT_DATABASE()"
         )
 
 
