151edf81f3ace13f23dd98656bb4e9053ec498fe
Edson <edsoncezar16@gmail.com>
2025-07-15 01:09:43 -0300

    feat: enable source tests as asset checks

--- orchestration/defs/transformation.py
+++ orchestration/defs/transformation.py
@@ -1,5 +1,11 @@
 from pathlib import Path
-from dagster_dbt import DbtCliResource, DbtProject, dbt_assets, DagsterDbtTranslator
+from dagster_dbt import (
+    DbtCliResource,
+    DbtProject,
+    dbt_assets,
+    DagsterDbtTranslator,
+    DagsterDbtTranslatorSettings,
+)
 import dagster as dg
 from typing import Mapping, Any, Optional
 
@@ -23,7 +29,9 @@ intrinsic_project.prepare_if_dev()
 
 @dbt_assets(
     manifest=intrinsic_project.manifest_path,
-    dagster_dbt_translator=CustomDagsterDbtTranslator(),
+    dagster_dbt_translator=CustomDagsterDbtTranslator(
+        settings=DagsterDbtTranslatorSettings(enable_source_tests_as_checks=True)
+    ),
 )
 def intrinsic_dbt_assets(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
