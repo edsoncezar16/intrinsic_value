890a4c77ba9dc0fed2a5bd4e77f1113606347177
Edson <edsoncezar16@gmail.com>
2025-07-15 16:05:25 -0300

    Revert "feat: setup for app runner"
    This reverts commit bdac392ebf8ce8cb7cc647786909afb9325ebab2.

--- app/helpers/load.py
+++ app/helpers/load.py
@@ -1,16 +1,12 @@
 import streamlit as st
 from streamlit.connections import SQLConnection
 from .config import QUERY_TTL, CONN_TTL_SECONDS
-import os
 
 
 @st.cache_data(ttl=QUERY_TTL)
 def portfolio_data(
     conn: SQLConnection = st.connection(
-        name="intrinsic",
-        type="sql",
-        ttl=CONN_TTL_SECONDS,
-        url=os.getenv("MOTHERDUCK_URL"),
+        name="intrinsic", type="sql", ttl=CONN_TTL_SECONDS
     ),
 ):
     return conn.query(
